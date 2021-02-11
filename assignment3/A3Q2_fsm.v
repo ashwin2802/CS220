3697e4ff34a536a45226b079c7a6ceb18aa11507
Rishabh Kothary <rishabhkothary76@gmail.com>
2021-02-12 02:24:22 +0530

    Comments added

--- assignment3/A3Q2_fsm.v
+++ assignment3/A3Q2_fsm.v
@@ -1,26 +1,36 @@
 module fsm(x, clk, y);
     input x, clk;
-    output reg y;
+    output reg y; // connect a wire here to get the output
 
     reg [2:0] state, next_state;
 
     initial begin
-        state <= 3'b000;
-        next_state <= 3'b000;
-        y <= 1'b1;
+        state <= 3'b000;     // FSM consists of 6 states thus 3 bits needed to store them
+        next_state <= 3'b000; // To store the next_state temporarily to get the output for the new input
+        y <= 1'b1;  // Empty string considered alternating
     end
 
     always @(posedge clk) begin 
+         // Implementing the next state functions and saving temporarily
         next_state[2] = (~x & (state[0] | state[2])) | (state[2] & (state[1] | state[0])) | (state[1] & x);
         next_state[1] = (state[2] & state[1]) | (~state[2] & ~state[1] & x) | (state[2] & ~state[0] & x);
         next_state[0] = ~state[0] | state[2] | (state[1] & x) | (~state[1] & state[0] & ~x);
         
+        //Storing the output
         y = ~next_state[2] | next_state[1] | ~next_state[0];
 
+        // Changing the state of out FSM according to next stat function permanently
         state[2] <= next_state[2];
         state[1] <= next_state[1];
-        state[0] <= next_state[0];
+        state[0] <= next_state[0];  
+        
+/*      state[2] <= (~x & (state[0] | state[2])) | (state[2] & (state[1] | state[0])) | (state[1] & x);
+        state[1] <= (state[2] & state[1]) | (~state[2] & ~state[1] & x) | (state[2] & ~state[0] & x);
+        state[0] <= ~state[0] | state[2] | (state[1] & x) | (~state[1] & state[0] & ~x);
+ */
 
-        $display("t = %d: x - %d, y - %d", $time,x, y);
+        //$display("t = %d: x - %d, y - %d", $time,x, y); //Displaying the output
     end
+
+    //assign y = ~state[2] | state[1] | ~state[0];
 endmodule
--- assignment3/A3Q2_fsm_top.v
+++ assignment3/A3Q2_fsm_top.v
@@ -15,24 +15,26 @@ module fsm_top;
     initial begin
         forever begin
             #5
-            clk = ~clk;
+            clk = ~clk;     // Generating a clock
         end
     end
 
-    // always @(posedge clk) begin
-    //     $display("t = %d: x - %d, y - %d", $time, x, y);
-    // end
+      always @(posedge clk) begin
+        #0                                                 // Zero delay needed for correct output else a lag observed
+        $display("t = %d: x - %d, y - %d", $time, x, y);   //Displaying the outputs
+
+     end 
 
     initial begin
-        #3  x=0;
+        #3  x=0;                               //Test cases
 
         #10 x=1;
 
         #10 x=0;
 
-        #10 x=1;
+        #10 x=1;                               // Till now alternating input thus input should be 1 till here
 
-        #10 x=1;
+        #10 x=1;                               // First point non-alternating input recieved so ouput should 0 from here onwards
 
         #10 x=1;
 
