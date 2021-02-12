9d0807df6cec4250c3eafa5743d3035c12c7efc8
Ashwin Shenai <ashwins@iitk.ac.in>
2021-02-12 23:06:11 +0530

    Replace latches with FFs in RAM

--- assignment3/A3Q1_read.v
+++ assignment3/A3Q1_read.v
@@ -6,9 +6,9 @@ module dram_bank(clk, row_num, input_valid, output_data, output_valid);
 
     reg [31:0] data [15:0];
     reg [3:0] open_row;
-    reg [1:0] wait_cycles;   //Needed to keep a count of number of cycles to wait according to the case
-    reg r_en, w_en;      // w_en acts as a switch to whether to give out an output if wait_cycle is 0 
-                         // r_en acts as a switch needed to handle when intially row_num is high impedence state
+    reg wait_cycles, r_en, w_en;    // wait_cycles: keeps a count of number of cycles to wait after input cycle according to the case
+                                    // r_en acts as a switch needed to handle when intially row_num is high impedence state
+                                    // w_en acts as a switch to whether to give out an output if wait_cycle is 0 
 
     initial begin
         data[0] <= 32'd0;
@@ -29,46 +29,44 @@ module dram_bank(clk, row_num, input_valid, output_data, output_valid);
         data[15] <= 32'd15;
 
         open_row <= 4'bZZZZ;
-        wait_cycles <= 2'd1;
+        wait_cycles <= 2'b1;
         r_en <= 1'b0;
         w_en <= 1'b0;
     end
 
     always @(posedge clk) begin
         if (input_valid) begin
-            if (r_en == 1'b0) begin     //The case when no row is open
-                wait_cycles = 2'd1;     // Setting the cycles to wait = 1
-                open_row = row_num;
-                output_valid = 1'b0;
-                r_en = 1'b1;
-                w_en = 1'b1;
+            if (r_en == 1'b0) begin     // The case when no row is open
+                wait_cycles <= 1'd0;    // Setting the cycles to wait after this cycle = 0
+                r_en <= 1'b1;
+                w_en <= 1'b1;
+                open_row <= row_num;
+                output_valid <= 1'b0;
             end
-            else if (open_row == row_num) begin   //The case when the row reqeusted is equal to the open row
-                wait_cycles = 2'd0;    // Setting the cycles to wait = 0
-                w_en = 1'b1;
+            else if (open_row == row_num) begin    // The case when the row reqeusted is equal to the open row
+                output_data <= data[row_num];       // Directly fetch output and return
+                output_valid <= 1'b1;
             end
-            else begin                  // The case when the requested row is not equal to currently open row
-                wait_cycles = 2'd2;     // Setting the cycles to wait = 2
-                open_row = row_num;
-                output_valid = 1'b0;
-                w_en = 1'b1;
+            else begin                      // The case when the requested row is not equal to currently open row
+                wait_cycles <= 1'd1;        // Setting the cycles to wait after this cycle = 2
+                open_row <= row_num;    
+                output_valid <= 1'b0;
+                w_en <= 1'b1;
             end
-        end
-
-        if (wait_cycles > 0) begin
-            wait_cycles = wait_cycles - 1;   // A decrementing counter that goes till 0
-            output_valid = 1'b0;
-        end 
-        else if (w_en == 1'b1) begin         // When wait_cycles = 0 and w_en switch is turned on
-            output_data = data[row_num];
-            output_valid = 1'b1;
-            w_en = 1'b0;
         end
         else begin
-            output_valid = 1'b0;
+            if (wait_cycles > 1'd0) begin
+                wait_cycles <= wait_cycles - 1;     // A decrementing counter that goes till 0
+                output_valid <= 1'b0;
+            end
+            else if (w_en == 1'b1) begin            // When wait_cycles = 0 and w_en switch is turned on
+                output_data <= data[row_num];
+                output_valid <= 1'b1;
+                w_en <= 1'b0;
+            end
+            else begin
+                output_valid <= 1'b0;
+            end
         end
-
-        // $display("[RAM] t = %d: %d %d %d %d\n", $time, input_valid, output_data, output_valid, wait_cycles);
     end
-
 endmodule
--- assignment3/A3Q1_read_top.v
+++ assignment3/A3Q1_read_top.v
@@ -1,4 +1,4 @@
-`include "A3Q1_read.v"
+`include "read.v"
 
 module read_top;
 
@@ -9,77 +9,82 @@ module read_top;
 
     dram_bank RAM (clk, row_num, input_valid, output_data, output_valid);
 
-    always @(posedge clk & output_valid) begin
-        if(output_valid == 1'b1) begin
+    always @(posedge clk) begin
+        @(posedge output_valid) begin
             $display("t = %d: Row num - %d, Data - %d", $time, row_num, output_data);
         end
     end
 
     always begin
         #5
-        clk = ~clk;                   // Handling the clock and switching input_valid to 0 periodically as asked
+        clk <= ~clk;                   // Handling the clock and switching input_valid to 0 periodically as asked
         #5
-        clk = ~clk;
+        clk <= ~clk;
         #3
-        input_valid = 1'b0;
+        input_valid <= 1'b0;
         #2
-        clk = ~clk;
+        clk <= ~clk;
         #5
-        clk = ~clk;
+        clk <= ~clk;
         #3
-        input_valid = 1'b0;
+        input_valid <= 1'b0;
         #2
-        clk = ~clk;
+        clk <= ~clk;
         #5
-        clk = ~clk;
+        clk <= ~clk;
     end
 
     initial begin
-        clk = 1'b0;
-        input_valid = 1'b0;
+        clk <= 1'b0;
+        input_valid <= 1'b0;
         #300;
         $finish;
     end
 
     initial begin
         #3
-        row_num = 4'b0000;     // First test case sent at 3s, here no open row present so expected output at 15s after 1 cycle
-        input_valid = 1;
+        row_num <= 4'b0000;     // First test case sent at 3s, here no open row present so expected output at 15s after 1 cycle
+        input_valid <= 1;
 
         #30
-        row_num = 4'b1111;    //  Input at 33s and row_num not equal to open row so ouput should occur at 35+20 = 55s
-        input_valid = 1;
+        row_num <= 4'b1111;    //  Input at 33s and row_num not equal to open row so ouput should occur at 35+20 = 55s
+        input_valid <= 1;
         
         #30
-        row_num = 4'b1110;
-        input_valid = 1;
+        row_num <= 4'b1110;
+        input_valid <= 1;
         
         #30
-        row_num = 4'b1010;
-        input_valid = 1;
+        row_num <= 4'b1010;
+        input_valid <= 1;
         
         #30
-        row_num = 4'b1000;
-        input_valid = 1;
+        row_num <= 4'b1000;
+        input_valid <= 1;
         
         #30
-        row_num = 4'b1000;   // Input at 153s, as open_row = row_num output should occur at 155s (the same cycle)
-        input_valid = 1;
+        row_num <= 4'b1000;   // Input at 153s, as open_row = row_num output should occur at 155s (the same cycle)
+        input_valid <= 1;
         
         #30
-        row_num = 4'b1111;
-        input_valid = 1;
+        row_num <= 4'b1111;
+        input_valid <= 1;
         
         #30
-        row_num = 4'b0000;
-        input_valid = 1;
+        row_num <= 4'b0000;
+        input_valid <= 1;
         
         #30
-        row_num = 4'b0000;    // Input at 243s, as open_row = row_num output should occur at 245s (the same cycle)
-        input_valid = 1;
+        row_num <= 4'b0000;    // Input at 243s, as open_row = row_num output should occur at 245s (the same cycle)
+        input_valid <= 1;
         
         #30
-        row_num = 4'b1010;
-        input_valid = 1;
+        row_num <= 4'b1010;
+        input_valid <= 1;
     end
+
+    // initial begin
+    //     $dumpfile("read.vcd");
+    //     $dumpvars(0,read_top);
+    // end
 endmodule
