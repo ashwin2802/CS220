5a103c06d0129bfa62814936c0d83d2637a702be
Rishabh Kothary <rishabhkothary76@gmail.com>
2021-02-17 23:19:51 +0530

    Corrected code for A4Q2

--- assignment4/A4_Q2_worm.v
+++ assignment4/A4_Q2_worm.v
@@ -1,9 +1,10 @@
-module worm(in,clk,out);
+module worm(in,clk,out1,out2);
 
     input[5:0] in;
     input clk;
-    output reg[2:0] out;
-    reg[2:0] state[5:0];
+    output wire[5:0] out1;
+    output wire[5:0] out2;
+    reg[5:0] state[1:0];
     reg[1:0] dir;
     reg[3:0] size;
     reg[5:0] temp;
@@ -17,9 +18,11 @@ module worm(in,clk,out);
         dir <= {in[5],in[4]};
         size <= {in[3],in[2],in[1],in[0]};
         if(dir == 2'd0)begin
-            temp <= state[0]+size;
+            temp = state[0]+size;
+            //$display("temp = %d size = %d   ",temp,size);
             if(temp < 5'd15)begin
                 state[0] <= temp;
+                //$display("state[0] = %d\n",state[0]);
             end
             else begin
                 state[0] <= 5'd15;
@@ -27,7 +30,7 @@ module worm(in,clk,out);
         end
 
         else if(dir == 2'd1)begin
-            temp <= state[1]+size;
+            temp = state[1]+size;
             if(temp < 5'd15)begin
                 state[1] <= temp;
             end
@@ -36,7 +39,7 @@ module worm(in,clk,out);
             end
         end
         else if(dir == 2'd2)begin
-            temp <= state[0]-size;
+            temp = state[0]-size;
             if(temp >5'd0)begin
                 state[0] <= state[0]-size;
             end
@@ -45,7 +48,7 @@ module worm(in,clk,out);
             end
         end 
         else begin
-            temp <= state[1] - size;
+            temp = state[1] - size;
             if(temp >5'd0)begin
                 state[1] <= state[0] -size;
             end
@@ -54,6 +57,9 @@ module worm(in,clk,out);
             end
         end
      end
+
+     assign out1 = state[0];
+     assign out2 = state[1];
 
 
 
---
+++ assignment4/A4_Q2_wormtop.v
@@ -0,0 +1,39 @@
+`include "A4_Q2_worm.v"
+
+module worm_top;
+    reg clk;
+    reg[5:0] in;
+    wire[5:0] out1;
+    wire[5:0] out2;
+
+    worm a(in,clk,out1,out2);
+
+    always @(negedge clk) begin
+        $display("t = %d: Co-ordinates = (%d,%d)", $time, out1,out2);
+
+    end
+
+    always begin
+        #5
+        clk <= ~clk;
+    end
+
+    initial begin
+        clk <= 1'b0;
+        #200
+        $finish;
+    end  
+
+    initial begin
+        in <= 6'b000010;
+        #10
+        in <= 6'b000011;
+        #10
+        in <= 6'b010111;
+        #20
+        $finish;
+    end
+
+    
+
+endmodule
