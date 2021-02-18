c02f01ff49c313c2a35a315fa658848aaa8238e2
Rishabh Kothary <rishabhkothary76@gmail.com>
2021-02-18 22:40:40 +0530

    Testcases added for A4Q1

--- assignment4/A4Q1_top.v
+++ assignment4/A4Q1_top.v
@@ -14,27 +14,23 @@ module adder_top;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
-        A = 8'b00000000; B = 8'b01111111; opcode = 0;
+        A = 8'b00110000; B = 8'b01111111; opcode = 0;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
-        A = 8'b10000000; B = 8'b01111111; opcode = 0;
+        A = 8'b10101000; B = 8'b01111111; opcode = 0;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
-        A = 8'b10000000; B = 8'b01111111; opcode = 1;
+        A = 8'b00101010; B = 8'b01101011; opcode = 1;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
-        A = 8'b10000000; B = 8'b01111111; opcode = 0;
+        A = 8'b01010100; B = 8'b01111111; opcode = 1;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
-        A = 8'b10000000; B = 8'b01111111; opcode = 1;
-        #1
-        $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
-
-        A = 8'b00000000; B = 8'b01111111; opcode = 0;
+        A = 8'b00101000; B = 8'b01111111; opcode = 1;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
@@ -42,11 +38,15 @@ module adder_top;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
-        A = 8'b10000000; B = 8'b01111111; opcode = 1;
+        A = 8'b00001010; B = 8'b01100111; opcode = 1;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
-        A = 8'b10000000; B = 8'b01111111; opcode = 1;
+        A = 8'b00100100; B = 8'b11101111; opcode = 1;
+        #1
+        $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
+
+        A = 8'b01010000; B = 8'b01111111; opcode = 0;
         #1
         $display("t = %d: %b + %b + %b = %b, Carry = %b Over? = %b", $time, A, B, opcode, sum, c_out, over);
 
