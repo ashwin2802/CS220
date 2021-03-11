f8e91669d5e48ba22c83067e0427dd8b19d855bb
Rishabh Kothary <rishabhkothary76@gmail.com>
2021-03-11 20:56:20 +0530

    Question_1 solution added

---
+++ A5_Q1_Microcode_ROM.v
@@ -0,0 +1,27 @@
+module Microcode_ROM (current_state,clk,branch_control);
+    input[3:0] current_state;
+    input clk;
+    output reg[2:0] branch_control;
+    reg[2:0] rom [12:0];
+
+    initial begin
+        rom[0] = 3'd0;
+        rom[1] = 3'd0;
+        rom[2] = 3'd0;
+        rom[3] = 3'd1;
+        rom[4] = 3'd3;
+        rom[5] = 3'd3;
+        rom[6] = 3'd0;
+        rom[7] = 3'd0;
+        rom[8] = 3'd0;
+        rom[9] = 3'd0;
+        rom[10] = 3'd2;
+        rom[11] = 3'd4;
+        rom[12] = 3'd4;
+        branch_control = 3'd0;
+    end 
+
+    always @(posedge clk ) begin
+        branch_control <= rom[current_state];
+    end
+endmodule
