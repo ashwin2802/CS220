332955360100406bb72d5ccbf42380c575aa75b2
Ashwin Shenai <ashwins@iitk.ac.in>
2021-03-12 11:20:43 +0530

    Move A5Q1 files

---
+++ assignment5/A5Q1_FSM.v
@@ -0,0 +1,22 @@
+`include "A5Q1_Microcode_ROM.v"
+`include "A5Q1_Next_State.v"
+
+module FSM(clk,in,state);
+    input clk;
+    input[1:0] in;
+    output reg[3:0] state;
+    wire[2:0] branch_control;
+    wire[3:0] next;
+
+    initial begin
+        state <= 4'd0;
+    end
+
+    Microcode_ROM FSM_MR(state,clk,branch_control);
+
+    next_state FSM_next_state_func(state,clk,branch_control,next,in);
+    always @(negedge clk ) begin
+        state <= next;
+    end
+
+endmodule
