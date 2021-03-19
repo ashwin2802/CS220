63950976e3446785347347a99e681e57cf5de4b6
Rishabh Kothary <rishabhkothary76@gmail.com>
2021-03-19 23:05:51 +0530

    Except top_module all updated

--- assignment6_2/decoder.v
+++ assignment6_2/decoder.v
@@ -1,8 +1,7 @@
-module decoder(instruct,clk,read_address1,read_address2,write_address,write_in,opcode);
+module decoder(instruct,clk,read_address1,read_address2,write_address,opcode);
     input[33:0] instruct;
     input clk;
     output reg[4:0] read_address1,read_address2,write_address;
-    output reg[15:0] write_in;
     output reg[2:0] opcode;
 
     always @(posedge clk ) begin
@@ -10,7 +9,6 @@ module decoder(instruct,clk,read_address1,read_address2,write_address,write_in,o
         write_address <= instruct[30:26];
         read_address1 <= instruct[25:21];
         read_address2 <= instruct[20:16];
-        write_in <= instruct[15:0];
     end
 
 endmodule
--- assignment6_2/processor.v
+++ assignment6_2/processor.v
@@ -1,21 +1,20 @@
 `include "decoder.v"
 `include "register_file.v"
  
- module processor(instruct,clk,instruct_sig,output_sig);
+ module processor(instruct,clk,instruct_sig,output_sig,read_out1,read_out2,write_in);
     input[33:0] instruct;
     input clk,instruct_sig;
     output reg output_sig;
-
     wire[4:0] read_address1;
     wire[4:0] read_address2;
     wire[4:0] write_address;
-    wire[15:0] write_in;
-    wire[15:0] read_out1;
-    wire[15:0] read_out2;
+    output reg[15:0] write_in;
+    output wire[15:0] read_out1;
+    output wire[15:0] read_out2;
     wire[2:0] opcode;
     reg read_enable1,read_enable2,write_enable;
 
-    decoder decode_unit(instruct,clk,read_address1,read_address2,write_address,write_in,opcode);
+    decoder decode_unit(instruct,clk,read_address1,read_address2,write_address,opcode);
     register_file reg_file(read_address1,read_address2,write_address,read_enable1,read_enable2,write_enable,clk,write_in,read_out1,read_out2); 
     
     reg [4:0] counter;
@@ -28,6 +27,7 @@
     end
 
     always @(posedge clk) begin
+        write_in <= instruct[15:0];
         output_sig <= instruct_sig;
         if(instruct_sig==0) begin
             counter <= counter+1;
@@ -70,17 +70,53 @@
                 if(counter == 5'd2)begin
                     output_sig <= 1'b1;
                     read_enable1 <= 1'b0;
-                    
+                    read_enable2 <= 1'b0;
+                    write_enable <= 1'b0;
                 end 
             end
             else if(opcode == 3'b101) begin
-                
+                read_enable1 <= 1'b1;
+                read_enable2 <= 1'b1;
+                write_enable <= 1'b0;
+                if(counter == 5'd18)begin
+                    write_in <= read_out1 + read_out2;
+                    write_enable <= 1'b1;
+                end
+                if(counter == 5'd20)begin
+                    output_sig <= 1'b1;
+                    read_enable1 <= 1'b0;
+                    read_enable2 <= 1'b0;
+                    write_enable <= 1'b0;
+                end
             end
             else if(opcode == 3'b110) begin
-                
+                read_enable1 <= 1'b1;
+                read_enable2 <= 1'b1;
+                write_enable <= 1'b0;
+                if(counter == 5'd18)begin
+                    write_in <= read_out1 - read_out2;
+                    write_enable <= 1'b1;
+                end
+                if(counter == 5'd20)begin
+                    output_sig <= 1'b1;
+                    read_enable1 <= 1'b0;
+                    read_enable2 <= 1'b0;
+                    write_enable <= 1'b0;
+                end
             end
             else begin
-                
+                read_enable1 <= 1'b1;
+                write_enable <= 1'b0;
+                if(counter == 5'd18)begin
+                    write_in <= read_out1 << write_in;
+                    write_enable <= 1'b1;
+                end
+                if(counter == 5'd20)begin
+                    output_sig <= 1'b1;
+                    read_enable1 <= 1'b0;
+                    read_enable2 <= 1'b0;
+                    write_enable <= 1'b0;
+                end
             end
             
         end
