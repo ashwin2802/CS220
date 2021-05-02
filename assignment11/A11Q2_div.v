778fb1506153684dbf6a6c970b49243f296edc8c
Ashwin Shenai <ashwins@iitk.ac.in>
2021-05-02 23:56:37 +0530

    Pre-submission modifications for Assignment 11

--- assignment11/A11Q2_div.v
+++ assignment11/A11Q2_div.v
@@ -1,54 +1,57 @@
-module divider(done,clk,old_remainder,old_quotient,old_divisor,old_counter,divider_size,dividend_size,new_remainder,new_quotient,new_divisor,new_counter,status);
-    input [31:0] old_remainder,old_quotient,old_divisor;
-    input [4:0]divider_size,dividend_size,old_counter;
-    input clk,done;
-    output reg [31:0] new_remainder,new_quotient,new_divisor;
-    output reg [4:0] new_counter;
-    output reg status;
+module divider(clk,inp,dividend,dividend_length,divisor,divisor_length,quotient,remainder,add_count,sub_count,done);
+    input clk,inp;
+    input[31:0] dividend,divisor;
+    input[4:0] dividend_length,divisor_length;
+
+    output reg done;
+    output reg [4:0] add_count,sub_count;
+    output reg [31:0] quotient,remainder;
+    
+    reg [31:0] div;
+    reg [5:0] counter;
 
     initial begin
         status <= 1'b0;
     end
 
     always @(posedge clk ) begin
-        if(done == 1'b0)begin
-            status <= 1'b1;
-            if(dividend_size - divider_size > 0)begin
-                new_remainder <= old_remainder;
-                new_divisor <= (old_divisor << (dividend_size - divider_size));
-                new_quotient <= old_quotient;
-                new_counter <= old_counter;
+        if(inp == 1'b1)begin  // fetch new input
+            remainder <= dividend;
+            quotient <= 31'd0;
+            if(dividend_length > divisor_length)begin
+                div <= divisor << (dividend_length - divisor_length);
             end
+            add_count <= 0;
+            sub_count <= 0;
+            counter <= 0;
+            done <=1'b0;
         end
-        else if(done == 1'b1)begin
-            if ((old_counter < (dividend_size - divider_size ))&& (status ==1'b1))begin
-                new_divisor <= old_divisor >> 1;
-                new_counter <= old_counter+1;
-                if(old_remainder[31] == 1'b1)begin
-                    new_remainder <= old_remainder + old_divisor;
-                    new_quotient = (old_quotient^(32'd1))<<1;
-                    new_quotient[0] = 1'b1;
-                end
-                else if(old_remainder[31] == 1'b0)begin
-                    new_remainder <= old_remainder - old_divisor;
-                    new_quotient = (old_quotient << 1);
-                    new_quotient[0] = 1'b1;
-                end
+        else if(inp == 1'b0)begin
+            if(dividend_length < divisor_length)begin
+                done <= 1'b1;
             end
-            else if((old_counter >= (dividend_size - divider_size)) && (status ==1'b1) )begin
-                if(old_remainder[31] == 1'b1)begin
-                    new_remainder <= old_remainder + old_divisor;
-                    new_quotient <= (old_quotient^(32'd1));
-                    new_counter <= old_counter+1;
-                    new_divisor <= old_divisor;
-                    $display("Extra round entered");
+            else if (counter < dividend_length-divisor_length + 1) begin
+                if (remainder[31] == 1'b0) begin  // Positive remainder : subtraction
+                    remainder = remainder - div;
+                    sub_count = sub_count + 1;
                 end
-                else if(old_remainder[31] == 1'b0)begin
-                    new_remainder <= old_remainder;
-                    new_quotient <= old_quotient;
-                    new_counter <= old_counter+1;
-                    new_divisor <= old_divisor;
-                    status <=1'b0;
+                else begin         // Negative remainder: addition 
+                    remainder = remainder +div;
+                    add_count = add_count+1;
+                    quotient = quotient ^ 1; // Correct quotient
+                end
+
+                quotient = (quotient<<1) | 1'b1; // Increment and shift quotient
+                div <= div>>1; // Move to next bit
+                counter = counter + 1;
+            
+                if (counter == dividend_length-divisor_length + 1) begin // Extra round
+                    if(remainder[31] == 1'b1) begin  // Negative remainder
+                        remainder = remainder + div;
+                        quotient = quotient^1;
+                        add_count = add_count+1;
+                    end
+                    done <= 1'b1;
                 end
             end
             else begin
--- assignment11/A11Q2_top.v
+++ assignment11/A11Q2_top.v
@@ -9,23 +9,83 @@ module div_top;
     wire [4:0] new_counter;
     wire status;
 
-
-    divider div(done,clk,old_remainder,old_quotient,old_divisor,old_counter,divider_size,dividend_size,new_remainder,new_quotient,new_divisor,new_counter,status);
-
+    divider div(clk,inp,dividend,dividend_length,divisor,divisor_length,quotient,remainder,add_count,sub_count,done);
 
     always @(negedge clk ) begin
-            old_remainder <= new_remainder;
-            old_divisor <= new_divisor;
-            old_counter <= new_counter;
-            old_quotient <= new_quotient;
-        if(status == 1'b1)begin
-            $display("t = %d: Divisor= %d Remainder = %d  Quotient = %d Count = %d",$time,new_divisor,$signed(new_remainder),new_quotient,new_counter);
+        if(done ==1)begin
+            if(index !=0)begin
+                $display("Time =%d : Dividend = %d Divisor = %d Remainder = %d Quotient = %d Additions = %d Subtractions = %d",$time,dividend,divisor,remainder,quotient,add_count,sub_count);
+            end
+            index <= index+1;
+            inp <= 1'b1;
+            dividend <= dividend_list[index];
+            divisor <= divisor_list[index];
+            dividend_length <= dividend_length_list[index];
+            divisor_length <= divisor_length_list[index];
+            
+        end
+        else begin
+            inp <= 1'b0;
+        end
+
+        if(index > 4'd10)begin
+            $finish;
         end
     end
 
     initial begin
-        clk <= 1'b1;
-        done <= 1'b0;
+        index <=1'b0;
+        inp <= 1'b1;
+
+        dividend_list[0] = 67;
+        divisor_list[0] = 14;
+        dividend_length_list[0] = 7;
+        divisor_length_list[0] = 4;
+
+        dividend_list[1] <= 68;
+        divisor_list[1] <= 14;
+        dividend_length_list[1] <= 7;
+        divisor_length_list[1] <= 4;
+
+        dividend_list[2] <= 99;
+        divisor_list[2] <= 14;
+        dividend_length_list[2] <= 7;
+        divisor_length_list[2] <= 4;
+
+        dividend_list[3] <= 13;
+        divisor_list[3] <= 77;
+        dividend_length_list[3] <= 4;
+        divisor_length_list[3] <= 7;
+
+        dividend_list[4] <= 87;
+        divisor_list[4] <= 16;
+        dividend_length_list[4] <= 7;
+        divisor_length_list[4] <= 4;
+
+        dividend_list[5] <= 78;
+        divisor_list[5] <= 11;
+        dividend_length_list[5] <= 7;
+        divisor_length_list[5] <= 4;
+
+        dividend_list[6] <= 57;
+        divisor_list[6] <= 12;
+        dividend_length_list[6] <= 6;
+        divisor_length_list[6] <= 4;
+
+        dividend_list[7] <= 67;
+        divisor_list[7] <= 4;
+        dividend_length_list[7] <= 7;
+        divisor_length_list[7] <= 3;
+
+        dividend_list[8] <= 6;
+        divisor_list[8] <= 14;
+        dividend_length_list[8] <= 3;
+        divisor_length_list[8] <= 4;
+
+        dividend_list[9] <= 156;
+        divisor_list[9] <= 69;
+        dividend_length_list[9] <= 8;
+        divisor_length_list[9] <= 7;
     end
 
     initial begin
@@ -35,46 +95,9 @@ module div_top;
         end
     end
 
-    initial begin
-        
-        old_remainder <= 32'd67;
-        old_quotient <= 32'd0;
-        old_divisor <= 32'd14;
-        old_counter <= 5'd0;
-        dividend_size <= 5'd7;
-        divider_size <= 5'd4;
-        #10
-        done <= 1'b1;
-        
-/*         #95
-        done <=1'b0;
-        old_remainder <= 32'd99;
-        old_quotient <= 32'd0;
-        old_divisor <= 32'd15;
-        old_counter <= 5'd0;
-        dividend_size <= 5'd7;
-        divider_size <= 5'd4;
-        #5
-        done <= 1'b1; */
-    end
-
-    initial begin
-        #500
-        $finish;
-    end
-
-        // gtkwave debugging
-
-    initial begin
-        $dumpfile("divide.vcd");
-        $dumpvars(0, div_top);
-    end 
-
-
-
-
-
-
+    // initial begin
+    //     $dumpfile("div.top");
+    //     $dumpvars(0, div_top);
+    // end
 
 endmodule
-
