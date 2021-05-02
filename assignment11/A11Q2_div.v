6d74fbd12d6cfcbfb45a677a3fe97360e2058f8c
Ashwin Shenai <ashwins@iitk.ac.in>
2021-05-02 23:56:37 +0530

    Pre-submission modifications for Assignment 11

--- assignment11/A11Q2_div.v
+++ assignment11/A11Q2_div.v
@@ -1,46 +1,57 @@
-module divider(done,clk,old_remainder,old_quotient,old_divisor,old_counter,divider_size,dividend_size,new_remainder,new_quotient,new_divisor,new_counter,status);
-    input [31:0] old_remainder,old_quotient,old_divisor;
-    input [4:0]divider_size,dividend_size,old_counter;
-    input clk,done;
-    output reg [31:0] new_remainder,new_quotient,new_divisor;
-    output reg [4:0] new_counter;
-    output reg status;
+module divider(clk,inp,dividend,dividend_length,divisor,divisor_length,quotient,remainder,add_count,sub_count,done);
+    input clk,inp;
+    input[31:0] dividend,divisor;
+    input[4:0] dividend_length,divisor_length;
+
+    output reg done;
+    output reg [4:0] add_count,sub_count;
+    output reg [31:0] quotient,remainder;
+    
+    reg [31:0] div;
+    reg [5:0] counter;
 
     initial begin
         status <= 1'b0;
     end
 
     always @(posedge clk ) begin
-        if(done == 1'b0)begin
-            status <= 1'b1;
-            if(dividend_size - divider_size > 0)begin
-                new_remainder <= old_remainder;
-                new_divisor <= (old_divisor << (dividend_size - divider_size));
-                new_quotient <= old_quotient;
-                new_counter <= old_counter;
+        if(inp == 1'b1)begin  // fetch new input
+            remainder <= dividend;
+            quotient <= 31'd0;
+            if(dividend_length > divisor_length)begin
+                div <= divisor << (dividend_length - divisor_length);
             end
+            add_count <= 0;
+            sub_count <= 0;
+            counter <= 0;
+            done <=1'b0;
         end
         else if(inp == 1'b0)begin
-            if (counter > 0) begin
-                if($signed(remainder)<0)begin
+            if(dividend_length < divisor_length)begin
+                done <= 1'b1;
+            end
+            else if (counter < dividend_length-divisor_length + 1) begin
+                if (remainder[31] == 1'b0) begin  // Positive remainder : subtraction
+                    remainder = remainder - div;
+                    sub_count = sub_count + 1;
+                end
+                else begin         // Negative remainder: addition 
                     remainder = remainder +div;
                     add_count = add_count+1;
-                    quotient = quotient^1;
+                    quotient = quotient ^ 1; // Correct quotient
                 end
-                else begin
-                    remainder = remainder - div;
-                    sub_count = sub_count+1;
-                end
-                quotient = (quotient<<1) | 1'b1;
-                counter = counter -1;
-                div<= div>>1;
-                if(counter == 0)begin
-                    if($signed(remainder)<0)begin
+
+                quotient = (quotient<<1) | 1'b1; // Increment and shift quotient
+                div <= div>>1; // Move to next bit
+                counter = counter + 1;
+            
+                if (counter == dividend_length-divisor_length + 1) begin // Extra round
+                    if(remainder[31] == 1'b1) begin  // Negative remainder
                         remainder = remainder + div;
-                        quotient = quotient ^ 1;
+                        quotient = quotient^1;
                         add_count = add_count+1;
                     end
-                done <= 1'b1;
+                    done <= 1'b1;
                 end
             end
         end
--- assignment11/A11Q2_top.v
+++ assignment11/A11Q2_top.v
@@ -12,6 +12,10 @@ module div_top;
 
     divider div(done,clk,old_remainder,old_quotient,old_divisor,old_counter,divider_size,dividend_size,new_remainder,new_quotient,new_divisor,new_counter,status);
 
+<<<<<<< HEAD
+=======
+    divider div(clk,inp,dividend,dividend_length,divisor,divisor_length,quotient,remainder,add_count,sub_count,done);
+>>>>>>> Pre-submission modifications for Assignment 11
 
     always @(negedge clk ) begin
         if(done ==1)begin
@@ -30,14 +34,69 @@ module div_top;
             inp <= 1'b0;
         end
 
-        if(index > 10)begin
+        if(index > 4'd10)begin
             $finish;
         end
     end
 
     initial begin
+<<<<<<< HEAD
         clk <= 1'b1;
         done <= 1'b0;
+=======
+        index <=1'b0;
+        inp <= 1'b1;
+
+        dividend_list[0] = 67;
+        divisor_list[0] = 14;
+        dividend_length_list[0] = 7;
+        divisor_length_list[0] = 4;
+
+        dividend_list[1] <= 68;
+        divisor_list[1] <= 14;
+        dividend_length_list[1] <= 7;
+        divisor_length_list[1] <= 4;
+
+        dividend_list[2] <= 99;
+        divisor_list[2] <= 14;
+        dividend_length_list[2] <= 7;
+        divisor_length_list[2] <= 4;
+
+        dividend_list[3] <= 13;
+        divisor_list[3] <= 77;
+        dividend_length_list[3] <= 4;
+        divisor_length_list[3] <= 7;
+
+        dividend_list[4] <= 87;
+        divisor_list[4] <= 16;
+        dividend_length_list[4] <= 7;
+        divisor_length_list[4] <= 4;
+
+        dividend_list[5] <= 78;
+        divisor_list[5] <= 11;
+        dividend_length_list[5] <= 7;
+        divisor_length_list[5] <= 4;
+
+        dividend_list[6] <= 57;
+        divisor_list[6] <= 12;
+        dividend_length_list[6] <= 6;
+        divisor_length_list[6] <= 4;
+
+        dividend_list[7] <= 67;
+        divisor_list[7] <= 4;
+        dividend_length_list[7] <= 7;
+        divisor_length_list[7] <= 3;
+
+        dividend_list[8] <= 6;
+        divisor_list[8] <= 14;
+        dividend_length_list[8] <= 3;
+        divisor_length_list[8] <= 4;
+
+        dividend_list[9] <= 156;
+        divisor_list[9] <= 69;
+        dividend_length_list[9] <= 8;
+        divisor_length_list[9] <= 7;
+>>>>>>> Pre-submission modifications for Assignment 11
     end
 
     initial begin
@@ -46,6 +105,7 @@ module div_top;
             clk <= ~clk;
         end
     end
+<<<<<<< HEAD
 
     initial begin
         
@@ -89,4 +149,11 @@ module div_top;
 
 
 endmodule
+=======
+
+    // initial begin
+    //     $dumpfile("div.top");
+    //     $dumpvars(0, div_top);
+    // end
+>>>>>>> Pre-submission modifications for Assignment 11
 
